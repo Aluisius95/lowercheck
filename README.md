1a1e32db1cf2936eb160064fe25bac378cb56db7
Aluisius95 <120209413+Aluisius95@users.noreply.github.com>
2023-05-09 10:22:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lowercheck
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fv8pqs)
---
+++ index.js
@@ -0,0 +1,22 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<hr></hr>`;
+var books = [
+  { autore: 'Manzoni', titolo: 'I Promessi Sposi', anno: '1827' },
+  { autore: 'Tolkien', titolo: 'Il Signore degli Anelli', anno: '1954' },
+];
+
+var string = 'sig';
+
+for (var i = 0; i < books.length; i++) {
+  check(books[i]);
+}
+
+function check(book) {
+  var concat = book.autore.toLowerCase() + ' ' + book.titolo.toLowerCase();
+  appDiv.innerHTML +=
+    '<p>' + concat + '<br>' + concat.includes(string) + '</p>';
+}
